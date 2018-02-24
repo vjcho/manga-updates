9de052b6d42be0ac76a610ba503face03da32435
vjcho <vjcho@ucdavis.edu>
2018-02-23 21:41:04 -0800

    Display saved manga

--- manga.js
+++ manga.js
@@ -1,4 +1,8 @@
 
+function appendManga(manga) {
+    document.getElementById("mangaList").innerHTML += "<p>" + manga + "</p>";
+}
+
 function saveManga() {
     var mangaTitle = document.getElementById("mangaTitle").value;
 
@@ -9,8 +13,10 @@ function saveManga() {
           "read": true
         };
 
-        chrome.storage.sync.set(data);
-    };)
+        chrome.storage.sync.set(data, function() {
+            appendManga(mangaTitle);
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -20,5 +26,12 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.sync.get(null, function(data) {
         if (!('mangaUpdates' in data))
             chrome.storage.sync.set({"mangaUpdates": {}});
-    })
+        else {
+            // Display saved manga
+            for (var manga in data['mangaUpdates']) {
+                console.log(manga);
+                appendManga(manga);
+            }
+        }
+    });
 });
