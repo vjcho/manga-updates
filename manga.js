8de8da59f6e05520a5c08a09a340337db1c0bed2
vjcho <vjcho@ucdavis.edu>
2018-02-23 21:12:57 -0800

    Add function to save manga titles to sync storage

--- manga.js
+++ manga.js
@@ -1,9 +1,24 @@
 
 function saveManga() {
-  var mangaTitle = document.getElementById("mangaTitle").value;
-  console.log(mangaTitle);
+    var mangaTitle = document.getElementById("mangaTitle").value;
+
+    // Save title to sync storage
+    chrome.storage.sync.get("mangaUpdates", function(data) {
+        data["mangaUpdates"][mangaTitle] = {
+          "latestChapter": 0,
+          "read": true
+        };
+
+        chrome.storage.sync.set(data);
+    };)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("saveButton").addEventListener("click", saveManga);
+    document.getElementById("saveButton").addEventListener("click", saveManga);
+
+    // If no mangaUpdates obj, create new one
+    chrome.storage.sync.get(null, function(data) {
+        if (!('mangaUpdates' in data))
+            chrome.storage.sync.set({"mangaUpdates": {}});
+    })
 });
