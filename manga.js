b54bad0f9c0e8f143ab69f12b6fa5bdc233b1db4
vjcho <vjcho@ucdavis.edu>
2018-02-24 22:00:39 -0800

    Take manga url as input instead of title and save more info in manga object

--- manga.js
+++ manga.js
@@ -1,30 +1,48 @@
 
-function appendManga(title, manga) {
-    $("#mangaList").append("<p>" + title + " - Ch " + manga['latestChapter'] + "</p>");
+function appendManga(manga) {
+    $("#mangaList").append('<a href="http:' + manga['latestChapterLink'] + '">' + manga['title'] + ' - Ch' + manga['latestChapter'] + "</a><br>");
+}
+
+function getMangaData(mangaPage) {
+    var mangaObj = {};
+
+    console.log("Getting manga data");
+    var mangaTitle = $(mangaPage).find("div h1.title").text();
+
+    var latestChapterData = $(mangaPage).find(".detail_list ul li").first().find("a");
+    var latestChapterLink = $(latestChapterData).attr("href");
+    var latestChapter = $(latestChapterData).text().trim().split(" ").pop().trim();
+
+    mangaObj["title"] = mangaTitle;
+    mangaObj["latestChapterLink"] = latestChapterLink;
+    mangaObj["latestChapter"] = latestChapter;
+
+    console.log(mangaObj);
+    return mangaObj;
 }
 
 function saveManga() {
-    var mangaTitle = $("#mangaTitle").val();
+    var mangaUrl = $("#mangaUrl").val();
 
-    // Save title to sync storage
-    chrome.storage.sync.get("mangaUpdates", function(data) {
-        mangaObj = {
-          "latestChapter": 0,
-          "read": true
-        };
+    // Get data from url
+    $.get(mangaUrl, function(response) {
+        var mangaObj = getMangaData(response);
+        mangaObj['mangaLink'] = mangaUrl;
 
-        data["mangaUpdates"][mangaTitle] = mangaObj;
+        // Save title to sync storage
+        chrome.storage.sync.get("mangaUpdates", function(data) {
+            data["mangaUpdates"][mangaUrl] = mangaObj;
 
-        chrome.storage.sync.set(data, function() {
-            appendManga(mangaTitle, mangaObj);
+            chrome.storage.sync.set(data, function() {
+                appendManga(mangaObj);
+            });
         });
     });
 }
 
 function updateMangaList(mangaUrl) {
-    // Get around CORS
-    var url = "https://crossorigin.me/" + mangaUrl;
-    $.get(url, function(response) {
+    $.get(mangaUrl, function(response) {
+        console.log("Updating manga");
         var manga_updates = $(response).find(".manga_updates").children();
 
         manga_updates.each(function() {
@@ -46,15 +64,21 @@ function updateMangaList(mangaUrl) {
 $(document).ready(function() {
     $("#saveButton").click(saveManga);
 
+    $('#mangaList').on('click', 'a', function(){
+        chrome.tabs.create({url: $(this).attr('href')});
+        return false;
+   });
+
     // If no mangaUpdates obj, create new one
     chrome.storage.sync.get(null, function(data) {
+        console.log(data);
         if (!('mangaUpdates' in data))
             chrome.storage.sync.set({"mangaUpdates": {}});
         else {
             // Display saved manga
             var mangaList = data['mangaUpdates'];
             for (var manga in mangaList) {
-                appendManga(manga, mangaList[manga]);
+                appendManga(mangaList[manga]);
             }
         }
     });
